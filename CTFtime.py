c2d5b8ac9d20b5157505ab30c85a32879ed72868
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2023-04-23 01:21:36 +0200

    handle file creation bug

--- CTFtime.py
+++ CTFtime.py
@@ -2,6 +2,7 @@ import requests
 from hashlib import md5
 
 # constant variables
+savefile = "CTFtime_hash.dat"
 root = ''        # for automation purposes
 url = 'https://ctftime.org/team/196656'
 headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
@@ -18,10 +19,15 @@ if r.status_code == 200:
             tmp += line + '\n'
     # generate new hash
     newhash = md5(tmp.encode('utf-8')).hexdigest()
-    with open (root + "CTFtime_hash.dat", "r") as f:
-        # check against old hash
+    try:
+        open(root + savefile, "r")
+    except FileNotFoundError:
+        open(root + savefile, "x")
+    # check against old hash
+    with open(root + savefile, 'r')as f:
         if f.readline() != newhash:
-            w = open(root + "CTFtime_hash.dat", "w").write(newhash)
             print("[%] CTFtime Rank: Ng00m4lDhuhur rank changed\thttps://ctftime.org/team/196656")
+            open(root + "CTFtime_hash.dat", "w").write(newhash)
+    f.close()
 else:
     print("[X] Check Failed")
